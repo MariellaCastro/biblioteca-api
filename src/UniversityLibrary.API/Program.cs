83f162f1d4ba278331ef818076b8c5fbcf26e271
Mariella Castro <i202411929@cibertec.edu.pe>
2025-12-11 14:41:51 -0500

    Completion of cuestion 2 and correction of errors

--- src/UniversityLibrary.API/Program.cs
+++ src/UniversityLibrary.API/Program.cs
@@ -1,6 +1,32 @@
+using DotNetEnv;
+using UniversityLibrary.Application;
+using UniversityLibrary.Infrastructure;
+
+// Cargar variables de entorno desde .env
+var currentDir = Directory.GetCurrentDirectory();
+var envPath = Path.Combine(currentDir, ".env");
+
+if (!File.Exists(envPath))
+{
+    envPath = Path.Combine(currentDir, "..", "..", ".env");
+}
+
+if (File.Exists(envPath))
+{
+    Env.Load(envPath);
+    Console.WriteLine($".env file loaded from: {envPath}");
+    Console.WriteLine(envPath.ToString());
+}
+else
+{
+    Console.WriteLine(".env file not found.");
+}
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddInfrastructure();
+
+builder.Services.AddApplication();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
